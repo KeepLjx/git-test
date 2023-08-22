98ee4be96bc6c13ec50669eba378bb7dfcfa2337
Keepl <1431286186@qq.com>
2023-08-22 22:02:51 +0800

    push commit

--- src/main/java/com/jiaxin/GitTest.java
+++ src/main/java/com/jiaxin/GitTest.java
@@ -1,11 +1,6 @@
 package com.jiaxin;
 
-/**
- * @author:CookieBoy
- * @Description
- * @create ${DATE}
- * @version:1.0
- */
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
@@ -14,5 +9,6 @@ public class GitTest {
         System.out.println("Hello git4!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
